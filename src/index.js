60d7e8310701ff1d11c1b7d4b1bdbcedad3f6a3f
Karen Gil <albanis21@gmail.com>
2021-05-06 16:06:21 -0400

    cart items persistance with redux-persist

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Router>
     </Provider>
   </React.StrictMode>,
